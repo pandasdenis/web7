92928213c5030cefe19cd2d216fb3436afe92cc7
DenisPandas <95372773+DenisPandas@users.noreply.github.com>
2021-12-23 02:56:00 +0300

    Add files via upload

---
+++ README.js
@@ -0,0 +1,18 @@
+$(function(){
+    $(.slider).slick({
+        infinite true,
+        slidesToShow 4,
+        slidesToScroll 4,
+        dots true,
+        responsive [
+            {
+                breakpoint 480,
+                settings {
+                    slidesToShow 2,
+                    slidesToScroll 2
+                }
+            }
+        ]
+    });
+});
+
