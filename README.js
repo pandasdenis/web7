5bcd79fe5bff35184edef1e93ba14b289c43e25f
DenisPandas <95372773+DenisPandas@users.noreply.github.com>
2021-12-23 03:14:29 +0300

    Update README.js

--- README.js
+++ README.js
@@ -1,18 +1,19 @@
 $(function(){
-    $(.slider).slick({
-        infinite true,
-        slidesToShow 4,
-        slidesToScroll 4,
-        dots true,
-        responsive [
+    $(".slider").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        responsive: [
             {
-                breakpoint 480,
-                settings {
-                    slidesToShow 2,
-                    slidesToScroll 2
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             }
         ]
     });
 });
+
 
